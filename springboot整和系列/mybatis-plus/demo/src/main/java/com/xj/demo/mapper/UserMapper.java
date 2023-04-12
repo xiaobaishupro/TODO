c7e7194a7db953bd327bc116e70e65efed693b18
流浪 <1914896250@qq.com>
2023-04-13 03:51:02 +0800

    add mybatis-plus

---
+++ springboot整和系列/mybatis-plus/demo/src/main/java/com/xj/demo/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.xj.demo.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.xj.demo.pojo.User;
+
+/**
+ * @author 努力的派大星
+ * @version 1.0
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
